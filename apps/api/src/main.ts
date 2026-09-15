5f15b8f2cf82b4671ebe9ed229d533d0df3276ac
Zikra Daffa Saputra <zdsaputra@gmail.com>
2026-09-15 22:48:59 +0700

    install prisma

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT', 3000);
+
+  await app.listen(port);
 }
 
 await bootstrap();
